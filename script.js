6c5f1da773c6d799f85a75381912c9b1a7a0435f
Nitin Gill <129394957+nitin2644@users.noreply.github.com>
2024-07-26 22:26:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('funny-btn').addEventListener('click', function() {
-    window.location.href = 'funny.html'; // Link to funny portfolio page
-});
-
-document.getElementById('professional-btn').addEventListener('click', function() {
-    window.location.href = 'professional.html'; // Link to professional portfolio page
-});
